8193da7d4990e75680f7b4bd26c395026a44d1e2
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-26 17:06:53 +0700

    fix: generate barcode and vcard before insert in CreateContact

--- app/Filament/Resources/Contacts/Pages/CreateContact.php
+++ app/Filament/Resources/Contacts/Pages/CreateContact.php
@@ -26,50 +26,41 @@ class CreateContact extends CreateRecord
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['contactId'] = (string) Str::orderedUuid();
-        $data['barcode'] = '';
-        $data['file'] = '';
-
-        return $data;
-    }
-
-    protected function afterCreate(): void
-    {
-        $contact = $this->record;
 
         $vCardRaw = "BEGIN:VCARD\nVERSION:3.0\n"
-            ."N:{$contact->last_name};{$contact->first_name}\n"
-            ."FN:{$contact->first_name} {$contact->last_name}\n"
-            ."ADR;TYPE=WORK,PREF:;;{$contact->st_address};{$contact->city_address};{$contact->province_address};{$contact->postcode_address};{$contact->country_address}\n"
+            ."N:{$data['last_name']};{$data['first_name']}\n"
+            ."FN:{$data['first_name']} {$data['last_name']}\n"
+            ."ADR;TYPE=WORK,PREF:;;{$data['st_address']};{$data['city_address']};{$data['province_address']};{$data['postcode_address']};{$data['country_address']}\n"
             ."ORG:PT. Medquest Jaya Global\n"
-            ."ROLE:{$contact->dept}\n"
-            ."TITLE:{$contact->title}\n"
-            ."TEL;TYPE=MOBILE:{$contact->phone_number}\n"
-            ."TEL;TYPE=WORK:{$contact->phone_number2}\n"
-            ."EMAIL:{$contact->email}\n"
+            ."ROLE:{$data['dept']}\n"
+            ."TITLE:{$data['title']}\n"
+            ."TEL;TYPE=MOBILE:{$data['phone_number']}\n"
+            ."TEL;TYPE=WORK:{$data['phone_number2']}\n"
+            ."EMAIL:{$data['email']}\n"
             .'END:VCARD';
 
         $vcardObj = new VCard;
-        $vcardObj->addName($contact->last_name, $contact->first_name);
-        $vcardObj->addEmail($contact->email);
-        $vcardObj->addAddress(null, null, $contact->st_address, $contact->city_address, $contact->province_address, $contact->postcode_address, $contact->country_address, 'WORK');
-        $vcardObj->addPhoneNumber($contact->phone_number);
-        $vcardObj->addPhoneNumber($contact->phone_number2);
+        $vcardObj->addName($data['last_name'], $data['first_name']);
+        $vcardObj->addEmail($data['email']);
+        $vcardObj->addAddress(null, null, $data['st_address'], $data['city_address'], $data['province_address'], $data['postcode_address'], $data['country_address'], 'WORK');
+        $vcardObj->addPhoneNumber($data['phone_number']);
+        $vcardObj->addPhoneNumber($data['phone_number2']);
         $vcardObj->addCompany('PT. Medquest Jaya Global');
-        $vcardObj->addRole($contact->dept);
-        $vcardObj->addJobtitle($contact->title);
+        $vcardObj->addRole($data['dept']);
+        $vcardObj->addJobtitle($data['title']);
         $vcfContent = $vcardObj->getOutput();
 
         $qr = new DNS2D;
         $qr = base64_decode($qr->getBarcodePNG($vCardRaw, 'QRCODE'));
-        $barcodePath = 'img/vcard/'.$contact->contactId.'.png';
+        $barcodePath = 'img/vcard/'.$data['contactId'].'.png';
         Storage::disk('public')->put($barcodePath, $qr);
 
-        $vcfPath = 'file/vcard/'.$contact->first_name.'_'.$contact->last_name.'.vcf';
+        $vcfPath = 'file/vcard/'.$data['first_name'].'_'.$data['last_name'].'.vcf';
         Storage::disk('public')->put($vcfPath, $vcfContent);
 
-        $contact->update([
-            'barcode' => $barcodePath,
-            'file' => $vcfPath,
-        ]);
+        $data['barcode'] = $barcodePath;
+        $data['file'] = $vcfPath;
+
+        return $data;
     }
 }
