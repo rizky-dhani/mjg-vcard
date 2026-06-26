7fc956400b232ba205de65aa8c4e33863ec1d426
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-26 14:22:10 +0700

    fix: align labels left and values right matching button padding

--- resources/views/livewire/contacts/contact-detail.blade.php
+++ resources/views/livewire/contacts/contact-detail.blade.php
@@ -18,61 +18,37 @@
                             <hr width="40%" class="mx-auto">
                         </div>
                     </div>
-                    <div class="mb-0 card-body">
-                        <div class="row d-flex justify-content-center">
-                            <div class="participant-table d-flex justify-content-center">
-                                <div class="row">
-                                    <div class="text-center text-lg-start col-lg-5">
-                                        <p class="pb-0 mb-0 fw-bolder h6">
-                                            {{ __('Phone') }}
-                                        </p>
-                                    </div>
-                                    <div class="mb-3 text-center text-lg-start col-lg-7">
-                                        <a href="tel:{{ $contact->phone_number }}" class="text-black text-decoration-none">
-                                            <p class="h6 fw-normal">{{ $contact->phone_number }}</p>
-                                        </a>
-                                    </div>
-                                    @if ($contact->phone_number2 != null)
-                                        <div class="text-center text-lg-start col-lg-5">
-                                            <p class="pb-0 mb-0 fw-bolder h6">
-                                                {{ __('Phone 2') }}
-                                            </p>
-                                        </div>
-                                        <div class="mb-3 text-center text-lg-start col-lg-7">
-                                        <a href="tel:{{ $contact->phone_number2 }}" class="text-black text-decoration-none">
-                                            <p class="h6 fw-normal">{{ $contact->phone_number2 }}</p>
-                                        </a>
-                                        </div>
-                                    @endif
-                                    <div class="text-center text-lg-start col-lg-5">
-                                        <p class="pb-0 mb-0 fw-bolder h6">
-                                            {{ __('Email') }}
-                                        </p>
-                                    </div>
-                                    <div class="mb-3 text-center text-lg-start col-lg-7">
-                                        <a href="mailto:{{ $contact->email }}" class="text-black text-decoration-none">
-                                        <p class="h6 fw-normal">{{ $contact->email }}</p>
-                                        </a>
-                                    </div>
-                                    <div class="text-center text-lg-start col-lg-5">
-                                        <p class="pb-0 mb-0 fw-bolder h6">
-                                            {{ __('Company') }}
-                                        </p>
-                                    </div>
-                                    <div class="mb-3 text-center text-lg-start col-lg-7">
-                                        <p class="h6 fw-normal">{{ __('PT. Medquest Jaya Global') }}</p>
-                                        <p class="h6 fw-normal">{{ $contact->dept .' - '. $contact->title }}</p>
-                                    </div>
-                                    <div class="text-center text-lg-start col-lg-5">
-                                        <p class="pb-0 mb-0 fw-bolder h6">
-                                            {{ __('Address') }}
-                                        </p>
-                                    </div>
-                                    <div class="mb-3 text-center text-lg-start col-lg-7">
-                                        <p class="h6 fw-normal pe-lg-4">{{ $contact->st_address.', '.$contact->city_address.', '.$contact->province_address.' '.$contact->postcode_address.', '.$contact->country_address }}</p>
-                                    </div>
-                                </div>
+                    <div class="mb-0 px-5 card-body">
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="pb-0 mb-0 fw-bolder h6">{{ __('Phone') }}</p>
+                            <a href="tel:{{ $contact->phone_number }}" class="text-black text-decoration-none text-end">
+                                <p class="h6 fw-normal">{{ $contact->phone_number }}</p>
+                            </a>
+                        </div>
+                        @if ($contact->phone_number2 != null)
+                            <div class="d-flex justify-content-between mb-3">
+                                <p class="pb-0 mb-0 fw-bolder h6">{{ __('Phone 2') }}</p>
+                                <a href="tel:{{ $contact->phone_number2 }}" class="text-black text-decoration-none text-end">
+                                    <p class="h6 fw-normal">{{ $contact->phone_number2 }}</p>
+                                </a>
                             </div>
+                        @endif
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="pb-0 mb-0 fw-bolder h6">{{ __('Email') }}</p>
+                            <a href="mailto:{{ $contact->email }}" class="text-black text-decoration-none text-end">
+                                <p class="h6 fw-normal">{{ $contact->email }}</p>
+                            </a>
+                        </div>
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="pb-0 mb-0 fw-bolder h6">{{ __('Company') }}</p>
+                            <div class="text-end">
+                                <p class="h6 fw-normal mb-0">{{ __('PT. Medquest Jaya Global') }}</p>
+                                <p class="h6 fw-normal">{{ $contact->dept .' - '. $contact->title }}</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="pb-0 mb-0 fw-bolder h6">{{ __('Address') }}</p>
+                            <p class="h6 fw-normal text-end mb-0">{{ $contact->st_address.', '.$contact->city_address.', '.$contact->province_address.' '.$contact->postcode_address.', '.$contact->country_address }}</p>
                         </div>
                     </div>
                     <div class="px-5 download-button d-grid">
